d5187a5a1911b9f78cce96fb83b5ba14b44291c7
saivamseedharraju <97018418+saivamseedharraju@users.noreply.github.com>
2022-10-26 17:11:13 +0530

    Update Main2.java

--- Main2.java
+++ Main2.java
@@ -1,6 +1,6 @@
 public class Main2 {
     public static void main(String[] args) {
-        
+        //SAI VAMSEEDHAR RAJU
         int n=14;
         System.out.println("--------------------------Q.NO.1--------------------------------");
         System.out.println();
